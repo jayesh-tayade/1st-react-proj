2d5870137c9f6cd15bb5ef23689361af9d3cb2e1
Jayesh Chandrakant Tayade <jayesht140905@gmail.com>
2026-02-27 07:42:55 +0530

    Resolved the errors in Login & AllPosts

--- mega-proj/src/components/pages/AllPosts.jsx
+++ mega-proj/src/components/pages/AllPosts.jsx
@@ -2,17 +2,24 @@ import React,{useState,useEffect} from 'react'
 import appwriteService from '../../appwrite/config'
 import {Container,PostCard} from '../../components'
 import { useNavigate } from 'react-router-dom'
+import {Query} from "appwrite"
+import { useSelector } from 'react-redux'
 
 
 function AllPosts() {
     const [posts,setPosts] =useState([])
+    const userData=useSelector(state=>state.auth.userData)
     useEffect(()=>{
-            appwriteService.getPosts().then((posts) => {
+        if(!userData) return;
+            appwriteService.getPosts([
+                Query.equal("status","active"),
+                Query.equal("userId",userData.$id)
+            ]).then((posts) => {
                 if(posts){
                 setPosts(posts.documents)
             }
         })
-    },[])
+    },[userData])
 
   return (
     <div className='w-full py-8'>
--- mega-proj/src/components/pages/Home.jsx
+++ mega-proj/src/components/pages/Home.jsx
@@ -35,7 +35,7 @@ function Home() {
                 <div className='flex flex-wrap'>
                     {posts.map((post) => (
                         <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
+                            <PostCard post={post} />
                         </div>
                     ))}
                 </div>
